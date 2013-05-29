3a17f223678ddacab5a55627ffbec197d4f6a738
Georg Jung <georgjung@acm.org>
2013-05-29 09:33:10 +0200

    Added the actual implementation for the sequential composition node... had forgotten that.

--- PseudoMachineCode/src/syntaxTree/SequentialNode.java
+++ PseudoMachineCode/src/syntaxTree/SequentialNode.java
@@ -5,25 +5,28 @@ package syntaxTree;
 
 /**
  * @author jung
- *
+ * 
  */
 public class SequentialNode extends StatementNode {
+	StatementNode s1, s2;
 
 	/**
 	 * @param location
 	 */
-	public SequentialNode(SourceLocator location) {
+	protected SequentialNode(SourceLocator location) {
 		super(location);
-		// TODO Auto-generated constructor stub
 	}
 
-	/* (non-Javadoc)
-	 * @see syntaxTree.StatementNode#execute()
-	 */
+	public SequentialNode(SourceLocator location, StatementNode s1,
+			StatementNode s2) {
+		super(location);
+		this.s1 = s1;
+		this.s2 = s2;
+	}
+
 	@Override
 	public void execute() throws Exception {
-		// TODO Auto-generated method stub
-
+		s1.execute();
+		s2.execute();
 	}
-
 }
